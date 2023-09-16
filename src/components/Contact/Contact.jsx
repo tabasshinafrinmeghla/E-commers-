7bd94daebbc1bb5b412237471d807f17db1fee25
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2023-09-16 14:48:59 +0600

    update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        <h1 className='Contact'> "Contact us" </h1>
+
+        <p className='Contact_text'>Welcome to our E-commerce platform. We value your feedback, inquiries, and concerns. Our dedicated support team is here to assist you. Please feel free to reach out to us through the following channels </p>
+      </div>
+      <div className='input_text'>
+        <input type="email" id="email" pattern=".+@globex\.com" size="50" required placeholder="Enter a gmail address" />
+        <input type="text" id="text" pattern=".+@globex\.com" size="50" required placeholder="Enter your phone number" />
+        <input type="text" id="feed" pattern=".+@globex\.com" size="50" required placeholder="your feed back" />
+        <button type="submit" id='submit'>Submit</button>
+      </div>
+      <div>
+
+        <h1 id='Business'> Business Hours:</h1>
+        <h4 className='time'>Monday to Friday: <span >10:00AM to 08;00 PM</span></h4>
+        <h4 className='time'>Saturday: <span >10:00AM to 04;00 PM</span></h4>
+        <h4 className='time'>Sunday: <span >10:00AM to 04;00 PM</span></h4>
+        
+
+        {/* <input type="time" name="time" id="time" placeholder='' />
+        Saturday: [Business Hours]
+        Sunday: [Business Hours] */}
+      </div>
+
+    </>
   );
 };
 
