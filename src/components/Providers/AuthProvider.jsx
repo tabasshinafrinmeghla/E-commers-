df859decad8aa0f23c9fa5ce51c6a3a22c24e08d
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2023-10-23 00:43:39 +0600

    added

--- src/components/Providers/AuthProvider.jsx
+++ src/components/Providers/AuthProvider.jsx
@@ -1,18 +1,34 @@
-import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from '../../Firebase/fire.config';
 
 export const AuthContext = createContext(null);
 /**
  * আমরা auth provider বানাইলাম 
  */
 
+// const auth = getAuth(app);
+/* app টা পরে ইমপোরট করতে হবে না হ্লে ইরর দিবে */
+
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-  const user = { email: 'Megh' }
+  // const user = { email: 'Megh' }
+
+  /**আমাদের যখন ইউজার থাকবে না তখন নাল দেখাবে ইউজার থাকলে দেখাবে এই জন্য আমরা useSate diye user  ব্যবহার করব */
+
+  const [user, setUser] = useState(null);
+
+  /**
+   * 
+*/
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
 
   const authInfo = {
-    user,
+    user, createUser
 
   }
   return (
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './Registration.css';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Providers/AuthProvider';
+import { Result } from 'postcss';
 
 const Registration = () => {
 
   // password same na hole error dibe 
-  const [error, setError] = useState()
+  const [error, setError] = useState('');
+  // const { createUser } = useContext(AuthContext);
+  const { createUser } = useContext(AuthContext);
 
 
   const handleSignUp = event => {
@@ -17,7 +21,17 @@ const Registration = () => {
     const password = form.password.value;
     const conformPassword = form.conformPassword.value;
     console.log(email, password, conformPassword)
-    // condition check korbe password same ki na 
+
+
+
+
+    /*
+    প্রত্যেক বার রিসেট করবে
+    */
+    setError('');
+
+
+    // condition check korbe password same ki na
     if (password !== conformPassword) {
       setError('please put on same password!')
 
@@ -33,6 +47,21 @@ const Registration = () => {
 
 
   }
+
+  /**
+   * এই খানে ফাইয়ার বেস ফাংসান ব্যবহার করা হয়ছে
+   * */
+  
+
+  createUser(email, password)
+    .then(result => {
+      const loggedUser = result.user;
+      console.log(loggedUser)
+    })
+    .catch(error => {
+      console.log(error)
+      setError(error.message)
+    })
 
   return (
 
