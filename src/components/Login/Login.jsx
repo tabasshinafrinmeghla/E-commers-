0807ea58e16c6bd2dddc903f3d14662ec03313d0
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2023-10-22 16:25:34 +0600

    update

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,7 +5,24 @@ import './Login.css';
 const Login = () => {
   return (
     <>
-      <h1>Log In</h1>
+      <div className='form-container'>
+        <h2 className='form-title'>Log in</h2>
+        <form>
+          <div className="from-control">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="" placeholder='email' required />
+          </div>
+          <div className="from-control">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="" placeholder='password' required />
+          </div>
+          <input className='btn-submit' type="submit" value="Login" />
+          
+        </form>
+
+
+
+      </div>
     </>
   );
 };
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -3,8 +3,27 @@ import './Registration.css';
 
 const Registration = () => {
   return (
-    <div>
-      <h2>Sign Up</h2>
+
+    <div className='form-container'>
+      <h2 className='form-title'>Sign In </h2>
+      <form>
+        <div className="from-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="" placeholder='email' required />
+        </div>
+        <div className="from-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" placeholder='password' required />
+        </div>
+        <div className="from-control">
+          <label htmlFor="password"> Conform Password</label>
+          <input type="password" name="password" id="" placeholder='conform password' required />
+        </div>
+
+        <input className='btn-submit' type="submit" value="Login" />
+
+      </form>
+
     </div>
   );
 };
